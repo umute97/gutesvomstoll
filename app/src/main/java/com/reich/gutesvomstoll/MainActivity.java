641853d7b28078b1aa09cbb1d0e137e9b6534f40
Umut Elicabuk <amiculus.umut@hotmail.de>
2019-07-23 20:17:47 +0200

    add menu inflater to populate toolbar

--- app/src/main/java/com/reich/gutesvomstoll/MainActivity.java
+++ app/src/main/java/com/reich/gutesvomstoll/MainActivity.java
@@ -1,6 +1,8 @@
 package com.reich.gutesvomstoll;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.google.android.material.tabs.TabLayout;
 import com.reich.gutesvomstoll.ui.main.FavsFragment;
@@ -40,8 +42,8 @@ public class MainActivity extends AppCompatActivity {
 
     private void setupViewPager(ViewPager viewPager) {
         ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
-        adapter.addFragment(new SoundsFragment(), "ONE");
-        adapter.addFragment(new FavsFragment(), "TWO");
+        adapter.addFragment(new SoundsFragment(), getString(R.string.sound_frag_tabtitle));
+        adapter.addFragment(new FavsFragment(), getString(R.string.favs_frag_tabtitle));
         viewPager.setAdapter(adapter);
     }
 
@@ -73,4 +75,25 @@ public class MainActivity extends AppCompatActivity {
             return mFragmentTitleList.get(position);
         }
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_search) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 }
